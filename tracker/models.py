534e607e8df34aeedc82a3ae363a617eb96d676b
Abbos <abboszafarxojaev@gmail.com>
2025-05-23 16:02:49 +0500

    to'liq cod

--- tracker/models.py
+++ tracker/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Expense(models.Model):
     name = models.CharField(max_length=100)  # foydalanuvchi ismi
     amount = models.DecimalField(max_digits=10, decimal_places=2)  # sarflagan pul
-    date = models.DateField()  # avtomatik sana
+    date = models.DateField(auto_now=True)  # avtomatik sana
 
     def __str__(self):
         return f"{self.name} - {self.amount} so'm - {self.date}"
